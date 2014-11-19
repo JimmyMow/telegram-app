50a91671b3c5d32065982ee95d2ac1259f232aed
Jack Mallers <jimmymowschess@gmail.com>
2014-11-19 15:24:35 -0600

    fixed nav component

--- app/components/navbar-object.js
+++ app/components/navbar-object.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'nav',
+  classNames: ['app-nav'],
+
   actions: {
     signOut: function() {
-
+      this.set('session.user', null);
     }
   }
 });
--- app/initializers/session.js
+++ app/initializers/session.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
-// export function initialize(container, application) {
-//   application.inject('route', 'session', 'session:main');
-//   application.inject('controller', 'session', 'session:main');
-// }
-
 export default {
   name: 'session',
 
