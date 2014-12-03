ee1a94d41739b5f0762c2c57c8ae93f3a166c076
Jack Mallers <jimmymowschess@gmail.com>
2014-12-02 19:52:38 -0600

    first attempt at specific posts hw

--- app/routes/user/posts.js
+++ app/routes/user/posts.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find("post");
+    // this.modelFor('user.id')
+    return this.store.find('post', { user: 'JimmyMow' });
   }
 });
