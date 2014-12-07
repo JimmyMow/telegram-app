6c87c1662c4b149e3db5786092747e21c6f03d23
Jack Mallers <jimmymowschess@gmail.com>
2014-12-07 17:29:19 -0600

    persistant sessions

---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel: function() {
+    var route = this;
+    var promise = this.store.find('user', {isAuthenticated: true});
+    return promise.then(function(users) {
+      if (users && users.get('length') > 0) {
+        var user = users.get('firstObject');
+        route.set('session.user', user);
+      } else {
+        route.set('session.user', null);
+      }
+      return users;
+    });
+  }
+});
