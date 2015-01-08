55e969824f87dfb58f557d6d0746b1842ba95d68
Jack Mallers <jimmymowschess@gmail.com>
2015-01-08 16:06:13 -0600

    implemented vlads feedback and fixed the post model and query

--- app/routes/user/posts.js
+++ app/routes/user/posts.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var user = this.modelFor('user');
-    return this.store.find('post', { user: user.id });
+    return this.store.find('post', { user: user.id, operation: "userProfile" });
   }
 });
